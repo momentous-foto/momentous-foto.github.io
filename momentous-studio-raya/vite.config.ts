dffb43a9c44d35e3fe3b1554be2f33d38a00256e
Syaqirah <snsyaqirah@gmail.com>
2026-01-25 11:24:58 +0800

    Enhance deployment workflow to include Raya build process and combine outputs

--- momentous-studio-raya/vite.config.ts
+++ momentous-studio-raya/vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/momentous-studio-raya/' : '/',
   server: {
     host: "::",
     port: 8080,
